fe4d9053d3af9a2742e06b9a9fc632874896a0f8
KarinaDVI <kbbouza@gmail.com>
2024-07-06 11:02:43 -0300

    Agregado base de datos

--- venta_entradas_db.sql
+++ venta_entradas_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-07-2024 a las 12:09:57
+-- Tiempo de generación: 06-07-2024 a las 16:01:09
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 7.4.28
 
@@ -24,6 +24,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `admins`
+--
+
+CREATE TABLE `admins` (
+  `id_ad` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `rol` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `compra`
+--
+
+CREATE TABLE `compra` (
+  `id_venta` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_show` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `shows`
 --
 
@@ -40,9 +66,10 @@ CREATE TABLE `shows` (
 --
 
 INSERT INTO `shows` (`id`, `nombre`, `img`, `fecha`, `descripcion`) VALUES
-(9, 'nuevo show', '/images/4196e1fe-c3ae-4bf6-bb41-f9d6bad8a3bf.jpg', '2024-08-03', 'algo'),
-(11, 'show1', '/images/pericos.jpg', '2024-08-11', 'long play'),
-(12, 'show1', '/images/kapanga.jpg', '2024-08-10', 'long play');
+(9, 'Calamity', '/images/4196e1fe-c3ae-4bf6-bb41-f9d6bad8a3bf.jpg', '2024-08-03', 'Algo esta llegando'),
+(11, 'Decadentes', '/images/logo-los-autenticos-decadentes.png', '2024-11-15', 'El regreso'),
+(12, 'Tini', '/images/flat,750x,075,f-pad,750x1000,f8f8f8.jpg', '2024-08-10', 'long play'),
+(14, 'Kapanga', '/images/kapanga.jpg', '2024-07-21', 'Proximamente');
 
 -- --------------------------------------------------------
 
@@ -63,11 +90,25 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id_user`, `username`, `ciudad`, `email`, `clave`) VALUES
-(2, 'testuser', 'caba', 'algo@algo.com', '$2a$08$yE3YC6sB2Sdu/c3EufAHce9vshanBPuB0yjiRr6OfkMQN51crJ1zq');
+(15, 'user7', 'Tres de febrero', 'karina.bouza@hotmail.com', '$2a$08$FnD5WRM/6IJyeTm5923mjODvoMidRVXWXqNmxHwI4xXF.ZOXoPzIe'),
+(16, 'user7', 'Tres de febrero', 'karina.bouza@hotmail.com', '$2a$08$WY5eHTDzvwxnoYqcGOnQG.4xixqLuZshoFDcAeDvMtuLVaPtH8t3S'),
+(17, 'user8', 'Tres de febrero', 'karina.bouza@hotmail.com', '$2a$08$T3BsmYA11J48fU6e/zDgOeuC.YL/KPpWQFbEq2MgfK7vH305bw9lW');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id_ad`);
+
+--
+-- Indices de la tabla `compra`
+--
+ALTER TABLE `compra`
+  ADD PRIMARY KEY (`id_venta`);
 
 --
 -- Indices de la tabla `shows`
@@ -86,16 +127,28 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id_ad` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `compra`
+--
+ALTER TABLE `compra`
+  MODIFY `id_venta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `shows`
 --
 ALTER TABLE `shows`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
