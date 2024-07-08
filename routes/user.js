ea63fba77bc31b084d1093ab283495654398b0e8
KarinaDVI <kbbouza@gmail.com>
2024-07-08 05:33:14 -0300

    Ahora se puede finalizar o cancelar compra. Falta implementar sistema de roles y permisos. Login y register de usuariode permiso único

--- routes/user.js
+++ routes/user.js
@@ -3,48 +3,40 @@ var router = express.Router();
 const connection = require("./../bbdd")
 
 
-//Arreglar para que traiga el usuario y sus carrito realizado
-
 router.get('/', function(req, res, next) {
-   
- connection.query( 'SELECT  DISTINCT(s.nombre) AS nombre, c.id_venta as id, estado_compra, s.fecha AS fecha, c.cantidad AS cantidad, s.precio AS precio, (c.cantidad * s.precio) AS total FROM compra as c INNER JOIN shows as s on c.id_show=s.id INNER JOIN users as u ON c.id_user=c.id_user ',
-  function(error, results, fields){
-        
-        if (error) throw error;
-        /* res.json({data: results}) */
-        /*  console.log(results) */
+    connection.query(
+      `SELECT DISTINCT(s.nombre) AS nombre, c.id_venta AS id, estado_compra, s.fecha AS fecha, c.cantidad AS cantidad, s.precio AS precio, (c.cantidad * s.precio) AS total, u.username AS username FROM compra AS c INNER JOIN shows AS s ON c.id_show = s.id INNER JOIN users AS u ON c.id_user = u.id_user`,
+      function(error, results, fields) {
+         if (error) throw error;
          res.render('dashboard', {
-            data:results,
-            mensaje: 'Finalizar Compra'
-        }); 
-    });
-
-});
-
-router.post('/finalizar/:id', function(req, res, next) {
-   /*  console.log(req.body);  */// Para depuración
-
-    let consulta =  `update compra set estado_compra='finalizado' where id_venta= '${req.params.id}'`
-
-    connection.query(consulta, function (error, results, fields) {
-        if (error) throw error;
-        res.render('mensaje', {
-            mensaje: 'Compra Finalizada Exitosamente'
-        });
-    });
-});
-router.post('/cancelar/:id', function(req, res, next) {
-   /*  console.log(req.body); // Para depuración */
-
-    let consulta =  `DELETE FROM compra where id_venta= '${req.params.id}'`
-
-    connection.query(consulta, function (error, results, fields) {
-        if (error) throw error;
-        res.render('mensaje', {
-            mensaje: 'Cancelaste tu compra'
-        });
-    });
-});
-  
-
+             data: results,
+             mensaje: 'Finalizar Compra'
+         }); 
+     });
+ });
+ 
+ router.post('/finalizar/:id', function(req, res, next) {
+    let fin=req.params.id
+     let consulta = `UPDATE compra SET estado_compra = 'finalizado' WHERE id_venta = '${fin}'`;
+ 
+     connection.query(consulta, function(error, results, fields) {
+         if (error) throw error;
+         res.render('mensaje', {
+             mensaje: 'Compra Finalizada Exitosamente'
+         });
+     });
+ });
+ 
+ router.post('/cancelar/:id', function(req, res, next) {
+    let cancel=req.params.id
+     let consulta = `DELETE FROM compra WHERE id_venta = '${cancel}'`;
+ 
+     connection.query(consulta, function(error, results, fields) {
+         if (error) throw error
+         res.render('mensaje', {
+             mensaje: 'Cancelaste tu compra'
+         });
+     });
+ });
+ 
   module.exports = router;
