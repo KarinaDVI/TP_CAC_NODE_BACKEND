54aa5fa255609b98c877d43ad71489439d157862
@KarinaDVI <kbbouza@gmail.com>
2024-07-15 20:53:21 -0300

    quitado el script viejo de la primera base

--- bbdd.js
+++ bbdd.js
@@ -2,9 +2,9 @@ var mysql      = require('mysql');
 
 var connection = mysql.createConnection({
     host: 'localhost',
-    user: 'Lucila',
-    password: 'Lucila79',
-    database: 'venta_entradasdb.sql'
+    user: 'root',
+    password: '',
+    database: 'venta_entradasdb'
 });
 
 connection.connect();
