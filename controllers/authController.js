9f3b2da5419630dc89498941e7de7909f3d4c07c
@KarinaDVI <kbbouza@gmail.com>
2024-07-19 00:12:49 -0300

    arreglando el register

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,34 +4,51 @@ const config = require('../config/config');
 const connection = require('../bbdd'); // Conexión a la base de datos
 
 exports.register = (req, res) => {
-  const { username, clave, ciudad, email } = req.body;
-
-  bcrypt.hash(clave, 8, (err, hashedPassword) => {
-    if (err) {
-      console.error('Error al cifrar la contraseña:', err);
+  const { username, clave, confirmarclave, ciudad, email } = req.body;
+  const query = `SELECT * FROM users WHERE username = ?`;
+  connection.query(query, [username], (error, results) => {
+    if (error) {
+      console.error('Error al ejecutar la consulta:', error);
       return res.status(500).send('Error interno al registrar usuario');
     }
+    
+    if (results.length === 0) {
+      if(confirmarclave===clave){
 
-    const password = hashedPassword;
-    const query = `INSERT INTO users (username, ciudad, email, clave) VALUES (?, ?, ?, ?)`;
-    connection.query(query, [username, ciudad, email, password], (error, results) => {
-      if (error) {
-        console.error('Error al ejecutar la consulta:', error);
-        return res.status(500).send('Error interno al registrar usuario');
+        bcrypt.hash(clave, 8, (err, hashedPassword) => {
+          if (err) {
+            console.error('Error al cifrar la contraseña:', err);
+            return res.status(500).send('Error interno al registrar usuario');
+          }
+
+          const password = hashedPassword;
+          const insertQuery = `INSERT INTO users (username, ciudad, email, clave) VALUES (?, ?, ?, ?)`;
+          
+          connection.query(insertQuery, [username, ciudad, email, password], (error, results) => {
+            if (error) {
+              console.error('Error al ejecutar la consulta:', error);
+              return res.status(500).send('Error interno al registrar usuario');
+            }
+
+            // Obtener el ID del usuario insertado
+            const userid = results.insertId;
+
+            // Generar el token JWT
+            const token = jwt.sign({ id: userid, username: username }, config.secretKey, { expiresIn: config.tokenExpiresIn });
+            
+            // Establecer la cookie con el token
+            res.cookie('authToken', token, { httpOnly: true });
+            
+            // Renderizar la vista con el mensaje de éxito
+            res.status(201).render('mensaje', { mensaje: 'Registro realizado exitosamente' });
+          });
+        });
+      }else{
+        return res.status(404).render('register', { mensaje: 'Las claves deben ser iguales'  });
       }
-
-      // Obtener el ID del usuario insertado
-      const userid = results.insertId;
-
-      // Generar el token JWT
-      const token = jwt.sign({ id: userid, username: username }, config.secretKey, { expiresIn: config.tokenExpiresIn });
-      
-      // Establecer la cookie con el token
-      res.cookie('authToken', token, { httpOnly: true });
-      
-      // Renderizar la vista con el mensaje de éxito
-      res.status(201).render('mensaje', { mensaje: 'Registro realizado exitosamente' });
-    });
+    } else {
+      return res.status(404).render('register', { mensaje: 'Usuario ya registrado' });
+    }
   });
 };
 
